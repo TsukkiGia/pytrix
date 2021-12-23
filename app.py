2d1a47b2e4353725db772dfd892f65cec87d98a8
Gianna Torpey <gtorpey@mit.edu>
2021-12-23 21:02:22 +0000

    Refactored app and consts

--- app.py
+++ app.py
@@ -13,21 +13,11 @@ December 6, 2021
 from consts import *
 from game2d import *
 from wave import *
-import math
-# from models import *
-
-
-# PRIMARY RULE: Invaders can only access attributes in wave.py via getters/setters
-# Invaders is NOT allowed to access anything in models.py
+from models import *
 
 class Pytrix(GameApp):
 
     def start(self):
-
-        # IMPLEMENT ME
-
-        # self.square = GImage(x=GAME_WIDTH/2, y=GAME_HEIGHT/2, width=60, height=60, source=SHIP_IMAGE)
-        # self.boxes = []
         self.time = 0
         self.piece = OPiece()
         self.last_keys = ()
@@ -41,22 +31,14 @@ class Pytrix(GameApp):
         if 'right' in self.input.keys and self.last_keys == ():
             for item in self.piece.blocks:
                 item.x += BLOCK_LENGTH
-            # self.square.x += 1
         if 'left' in self.input.keys and self.last_keys == ():
-            # self.square.x -= 1
             for item in self.piece.blocks:
                 item.x -= BLOCK_LENGTH
-        if 'up' in self.input.keys and self.last_keys == ():
-            # self.square.y += 1
-            for item in self.piece.blocks:
-                item.y += BLOCK_LENGTH
         if 'down' in self.input.keys and self.last_keys == ():
-            # self.square.y -= 1
             for item in self.piece.blocks:
                 item.y -= BLOCK_LENGTH
 
         self.last_keys = self.input.keys
-        print(len(self.done))
         if self.piece.canDrop():
             if self.time > 0.8:
                 for item in self.piece.blocks:
@@ -72,18 +54,7 @@ class Pytrix(GameApp):
             self.piece = OPiece()
 
     def draw(self):
-
-        # IMPLEMENT ME
-        # dark background
-
-        # for item in self.boxes:
-        #     item.draw(self.view)
-        # print(len(self.piece.blocks))
         for item in self.done:
             item.draw(self.view)
         for item in self.piece.blocks:
             item.draw(self.view)
-            # print(item)
-        # self.square.draw(self.view)
-
-    # HELPER METHODS FOR THE STATES GO HERE
