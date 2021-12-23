c5e78a3222628ea9ddff4ca329ecc995c1ece2a1
Gianna Torpey <gtorpey@mit.edu>
2021-12-23 22:04:34 +0000

    Added random piece selection

--- app.py
+++ app.py
@@ -2,15 +2,33 @@ from consts import *
 from game2d import *
 from wave import *
 from models import *
-
+import random
 
 class Pytrix(GameApp):
 
     def start(self):
         self.time = 0
-        self.piece = LPiece()
+        self.piece = self.pick_a_piece()
         self.last_keys = ()
         self.done = []
+
+    def pick_a_piece(self):
+        number = random.randint(0,6)
+        if number == 0:
+            return OPiece()
+        if number == 1:
+            return LPiece()
+        if number == 2:
+            return ZPiece()
+        if number == 3:
+            return IPiece()
+        if number == 4:
+            return JPiece()
+        if number == 5:
+            return TPiece()
+        if number == 6:
+            return SPiece()
+
 
     def update(self, dt):
         self.time += dt
@@ -23,7 +41,7 @@ class Pytrix(GameApp):
         else:
             for item in self.piece.blocks:
                 self.done.append(item)
-            self.piece = OPiece()
+            self.piece = self.pick_a_piece()
 
     def draw(self):
         for item in self.done:
